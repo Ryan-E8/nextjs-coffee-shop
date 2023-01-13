d405666196d185607933c62494ef3d8a3793b734
Ryan <ryanelliott.re1@gmail.com>
2023-01-12 21:06:46 -0600

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import Header from "../components/Header"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+            <Head>
+                <title>NFT Coffee Shop</title>
+                <meta name="description" content="NFT Coffee Shop" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider>
+                <Header> </Header>
+                <Component {...pageProps} />
+            </MoralisProvider>
+        </div>
+    )
 }
 
 export default MyApp
--- pages/burn-receipts.js
+++ pages/burn-receipts.js
@@ -3,5 +3,5 @@ import Image from "next/image"
 import styles from "../styles/Home.module.css"
 
 export default function Home() {
-    return <h1 className="py-4 px-4 font-bold text-2xl">This is some text</h1>
+    return <h1 className="py-4 px-4 font-bold text-2xl">This is some more text</h1>
 }
